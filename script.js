621175f44affd908f67e3959896a840675766fb3
eli bailey <baileyeli19@gmail.com>
2022-11-19 18:45:21 +0000

    class 7 labs

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 alert("Hello and welcome");
+
+        function sayHi() {
         let usersName = prompt ("what is your name?");
+        if (usersName == ""){
+            alert("I don't see a Name....");
+            usersName = prompt("Please tell me your name"); 
+        }
+
         usersName = usersName.toLocaleLowerCase();
         console.log("The user's name is: ", usersName);
         document.write("Hello and welcome to my page, "+ usersName);
+        }
 
         let response = prompt("do you like guitar's?")
         response = response.toLocaleLowerCase();
@@ -16,4 +24,3 @@ alert("Hello and welcome");
                 alert("I'm not sure what that means...");
 
             }
-            
